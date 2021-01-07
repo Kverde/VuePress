ad333322c8c51eab4c68dcdab0f4737a6fb71f55
KonstantinSh <Kverde@yandex.ru>
2021-01-07 15:09:57 +0500

    Пример добавления favicon

---
+++ favicon/docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  base: "/VuePress/",
+  head: [
+    ['meta', { name: "theme-color", content: "#ffffff"}],
+    ['link', { rel: "icon", href: "/assets/favicons/favicon.svg"}],
+    ['link', { rel: "mask-icon", href: "/assets/favicons/favicon.svg", color: "#000000"}],
+    ['link', { rel: "apple-touch-icon", href: "/assets/favicons/apple-touch-icon.png"}],
+    ['link', { rel: "manifest", href: "manifest.json"}]
+  ]
+}
